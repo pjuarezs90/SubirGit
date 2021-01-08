abf8408a4ae69f9ed3fb9829944d28f2f9378bb8
pjuarezs90 <pablo.juarez@atomic32.com>
2021-01-07 19:25:44 -0600

    Cambios

--- src/com/PrimerPruebaSelenium/PrimerPruebaSelenium.java
+++ src/com/PrimerPruebaSelenium/PrimerPruebaSelenium.java
@@ -5,6 +5,11 @@ public class PrimerPruebaSelenium {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		
+		for (int i = 0; i <= 30; i++) {
+			System.out.println("Hola "+i);
+			
+		}
 
 	}
 
